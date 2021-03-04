6e04c53e286a87d28e5b8a265d8caf4c4301b0d3
Jo-R <jo.rigg8@gmail.com>
2021-03-04 20:14:30 +0000

    add a bit of styling and slide in and out animation

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,8 +1,16 @@
 import React, { useState } from "react";
 import styles from "./Carousel.module.css";
 
+// TODO
+// ref: https://web.dev/carousel-best-practices/
+// and https://www.w3.org/TR/wai-aria-practices-1.2/#carousel
+
+// have it scroll automatically (must be able to stop it and screen reader should be apt)
+// slide picker control (ie the little dots to pick a slide)
+
 export const Carousel = ({ items }) => {
   const [activeItem, setActiveItem] = useState(0);
+  const [direction, setDirection] = useState("forward");
 
   const handleNext = () => {
     if (activeItem < items.length - 1) {
@@ -10,6 +18,7 @@ export const Carousel = ({ items }) => {
     } else {
       setActiveItem(0);
     }
+    setDirection("forward");
   };
 
   const handlePrev = () => {
@@ -18,17 +27,34 @@ export const Carousel = ({ items }) => {
     } else {
       setActiveItem(items.length - 1);
     }
+    setDirection("backward");
+  };
+
+  const getClassNameForItem = (isActive) => {
+    if (!isActive) {
+      return `${styles.item} ${styles.hidden}`;
+    } else {
+      if (direction === "forward") {
+        return `${styles.item} ${styles.active} ${styles.forward}`;
+      } else {
+        return `${styles.item} ${styles.active} ${styles.backward}`;
+      }
+    }
   };
 
   return (
-    <div>
-      <button onClick={handleNext}>Next</button>
-      <button onClick={handlePrev}>Prev</button>
+    <div className={styles.wrapper}>
       {items.map((el, i) => (
-        <div className={activeItem === i ? styles.active : styles.item} key={i}>
-          {el}
-        </div>
+        <div className={getClassNameForItem(activeItem === i)}>{el}</div>
       ))}
+      <div className={styles.btnWrapper}>
+        <button className={styles.scrollBtn} onClick={handlePrev}>
+          Prev
+        </button>
+        <button className={styles.scrollBtn} onClick={handleNext}>
+          Next
+        </button>
+      </div>
     </div>
   );
 };
